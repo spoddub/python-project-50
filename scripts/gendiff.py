ed536317ac6979ea618382763d3440cc2fa7be42
spoddub <spodd98@gmail.com>
2023-06-13 12:03:09 +0300

    commit

--- scripts/gendiff.py
+++ scripts/gendiff.py
@@ -2,7 +2,6 @@
 
 import argparse
 
-
 def main():
     parser = argparse.ArgumentParser(
         prog='gendiff',
@@ -10,10 +9,17 @@ def main():
     )
     parser.add_argument('first_file', help='First file')
     parser.add_argument('second_file', help='Second file')
+    parser.add_argument(
+        '-f', '--format',
+        help='Set format of output',
+        choices=['plain', 'json'],
+        default='plain'
+    )
 
     args = parser.parse_args()
     # Дополнительные действия, которые вы хотите выполнить при использовании скрипта
-
+    print(f'Comparing files: {args.first_file} and {args.second_file}')
+    print(f'Selected format: {args.format}')
 
 if __name__ == '__main__':
     main()
